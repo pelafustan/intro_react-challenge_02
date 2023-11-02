dcd7c06acd621f8cf810e8048d689132d4907c6f
Patricio Parada <paradagpatricio@gmail.com>
2023-11-02 11:19:06 -0300

    Update Registro.jsx
    What did I do?
      - generate an state to manage `Alerta` component.
      - Add `Formulario` to the view.

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,10 +1,14 @@
 import SocialButton from "./SocialButton";
 import Formulario from "./Formulario";
 import Alert from "./Alert";
+import { useState } from "react";
 
 const Registro = () => {
+  const [estadoAlerta, setEstadoAlerta] = useState("");
   return (
     <>
+      <Formulario setEstadoAlerta={setEstadoAlerta} />
+      <Alert estado={estadoAlerta} />
     </>
   )
 };
