b7355c4cb5ef6438c888790e964b53e846977b2b
Patricio Parada <paradagpatricio@gmail.com>
2023-11-02 11:13:29 -0300

    adding bootstrap stylesheet to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import './App.css'
-import React from 'react'
-import Registro from './components/Registro'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import React from 'react';
+import Registro from './components/Registro';
 
 function App() {
 
