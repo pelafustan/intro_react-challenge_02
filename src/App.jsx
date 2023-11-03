f06d49cd6485437037707745e09a5d28cd6ce723
Patricio Parada <paradagpatricio@gmail.com>
2023-11-03 18:06:56 -0300

    change place where alert event is generated, from Registro.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import React from 'react';
 import Registro from './components/Registro';
+import { useState } from "react";
 
 function App() {
-
+  // estado para configurar componente Alert: dentro de Formulario, estadoAlerta puede ser "success" o "danger"
+  const [estadoAlerta, setEstadoAlerta] = useState("");
   return (
     <>
-      <Registro />
+      <Registro estadoAlerta={estadoAlerta} setEstadoAlerta={setEstadoAlerta} />
     </>
   )
 }
