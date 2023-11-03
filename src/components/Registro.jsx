24c9e664c07119e806d57cb6e8428837afffa442
Patricio Parada <paradagpatricio@gmail.com>
2023-11-03 18:09:58 -0300

    update place where estadoAlerta y setEstadoAlerta are created

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,11 +1,8 @@
 import SocialButton from "./SocialButton";
 import Formulario from "./Formulario";
 import Alert from "./Alert";
-import { useState } from "react";
 
-const Registro = () => {
-  // estado para configurar componente Alert: dentro de Formulario, estadoAlerta puede ser "success" o "danger"
-  const [estadoAlerta, setEstadoAlerta] = useState("");
+const Registro = ({estadoAlerta, setEstadoAlerta}) => {
   return (
     <>
       <Formulario setEstadoAlerta={setEstadoAlerta} />
