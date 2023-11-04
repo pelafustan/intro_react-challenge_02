ed007f109cf64e8b5f51b0652381e2b7416e8b45
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2023-11-04 19:20:03 -0300

    Agrega alerta de bootstrap, props y condicionales

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,6 +1,19 @@
-const Alert = () => {
+import { useState } from 'react';
+import { Alert as Alerta } from 'react-bootstrap';
+
+const Alert = ({ estado }) => {
+  let mensaje = "";
+
+  if (estado == "danger") {
+    mensaje = "Completa todos los campos!";
+  }
+  if (estado == "success") {
+    mensaje = "Cuenta creada exitosamente!";
+  }
+  
   return (
     <>
+      {estado != "" ? <Alerta variant={estado}>{mensaje}</Alerta> : null}
     </>
   )
 };
