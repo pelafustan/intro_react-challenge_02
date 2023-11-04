43087a13aa3d21f47c35d16bfd9b8ec8e456a5f1
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2023-11-04 19:19:19 -0300

    Agrega íconos de fontawesome en componente socialbuttons

--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,7 +1,13 @@
-const SocialButton = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faFacebook, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
+const SocialButton = ({github, facebook, linkedin}) => {
   return (
-    <>
-    </>
+    <div id="icons">
+    {github && <FontAwesomeIcon icon={faGithub} size="2xl" />}
+    {facebook && <FontAwesomeIcon icon={faFacebook} size="2xl" />}
+    {linkedin && <FontAwesomeIcon icon={faLinkedinIn} size="2xl"/>}
+    </div>
   )
 };
 
